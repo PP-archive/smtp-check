1f563e1426c60952125a13df1cffd85f2cfc13e6
Pavel Polyakov <me@pavelpolyakov.com>
2014-02-08 23:51:51 +0200

    smtp check update

--- smtp-check.go
+++ smtp-check.go
@@ -2,18 +2,37 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/smtp"
 	"os"
+	"time"
 	/*"net/textproto"*/
 )
 
 func main() {
-	fmt.Printf("Hello world!\n")
+	fmt.Printf("Start!\n")
 
-	smtpHost := "smtp.yandex.ru"
+//	mxServers, err := net.LookupMX("yandex.ru")
+//
+//	if(err != nil) {
+//		fmt.Println("Error:", err)
+//		os.Exit(1)
+//	}
+//
+//	for key, mxServer := range mxServers {
+//		fmt.Println((key+1), ") OK:", mxServer.Host, mxServer.Pref)
+//	}
+//
+//	os.Exit(1)
+
+	smtpHost := "mx.yandex.ru"
 	smtpPort := "25"
 
-	Client, err := smtp.Dial(smtpHost+":"+smtpPort)
+	// to support timeout
+	timeout, _ := time.ParseDuration("10s")
+	Conn, err := net.DialTimeout("tcp", smtpHost+":"+smtpPort, timeout)
+
+	Client, err := smtp.NewClient(Conn, smtpHost)
 
 	if(err != nil) {
 		fmt.Println(err)
@@ -23,8 +42,20 @@ func main() {
 	fmt.Println("Connected to "+smtpHost+":"+smtpPort)
 
 	email1 := "ka-zaido@yandex.ru"
-	fmt.Println("1] Checking "+email1)
-	err = Client.Verify(email1)
+	//fmt.Println("1] Checking "+email1)
+	//err = Client.Verify(email1)
+
+	err = Client.Hello("hi")
+
+	if(err != nil) {
+		fmt.Println(err)
+	}
+	err = Client.Mail("pavel@kredito.de")
+
+	if(err != nil) {
+		fmt.Println(err)
+	}
+	err = Client.Rcpt(email1)
 
 	if(err != nil) {
 		fmt.Println(err)
@@ -33,9 +64,25 @@ func main() {
 		fmt.Println(email1+" is verified")
 	}
 
+	Client.Quit()
+	Client.Close()
+
+	// 2nd mail
+	Client, _ = smtp.Dial(smtpHost+":"+smtpPort)
+
 	email2 := "ka-z444aido@yandex.ru"
-	fmt.Println("2] Checking "+email2)
-	err = Client.Verify(email2)
+
+	err = Client.Hello("hi")
+
+	if(err != nil) {
+		fmt.Println(err)
+	}
+	err = Client.Mail("pavel@kredito.de")
+
+	if(err != nil) {
+		fmt.Println(err)
+	}
+	err = Client.Rcpt(email2)
 
 	if(err != nil) {
 		fmt.Println(err)
@@ -43,6 +90,9 @@ func main() {
 	} else {
 		fmt.Println(email2+" is verified")
 	}
+
+	Client.Quit()
+	Client.Close()
 
 	fmt.Println("Check complete")
 }
